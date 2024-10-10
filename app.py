2c119044d88919207f38ce20df21659c46c6bd6a
Saquib ishaq <124880268+Saquibishaq@users.noreply.github.com>
2024-10-11 01:25:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, jsonify
 from routes.video_routes import video_blueprint
+import os  # Make sure to import os
 
 app = Flask(__name__)
 app.register_blueprint(video_blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
