cf1ff5475a96767b67508748a6e945b7b709ffe6
Saquib ishaq <124880268+Saquibishaq@users.noreply.github.com>
2024-10-13 00:43:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ import os  # Make sure to import os
 app = Flask(__name__)
 app.register_blueprint(video_blueprint)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
