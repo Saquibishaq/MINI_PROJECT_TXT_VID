56fc01b8ba1ed0ff8a6a60ac35b2d56d67003a6a
Saquib ishaq <124880268+Saquibishaq@users.noreply.github.com>
2024-10-22 17:45:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify
-from routes.video_routes import video_blueprint
-import os  # Make sure to import os
+from flask import Flask
+from routes.video_routes import video_bp
 
 app = Flask(__name__)
-app.register_blueprint(video_blueprint)
+app.register_blueprint(video_bp)
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+# Ensure videos are saved in a 'static/videos' folder and Flask can serve them
+app.config['UPLOAD_FOLDER'] = 'static/videos'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9000)
